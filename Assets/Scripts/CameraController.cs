1c1a061ca7de80482174c68e64ee9a22cad05874
Desktop Mario Germoliato <mgermoliato@yahoo.com.br>
2022-07-12 20:32:34 -0300

    Implamentação da feature de zoom in e out na camera.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float zoomAmount;
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private CinemachineTransposer cinemachineTransposer;
+    private Vector3 targetFollowOffset;
+
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
     private void Update()
     {
-        MoveCamera();
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
     }
 
-    private void MoveCamera()
+    private void HandleMovement()
     {
         Vector3 inputMoveDirection = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
@@ -29,11 +46,14 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             inputMoveDirection.x = +1f;
-        }    
-
+        }
         Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
 
+    }
+
+    private void HandleRotation()
+    {      
         Vector3 rotationVector = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.Q))
         {
@@ -45,5 +65,19 @@ public class CameraController : MonoBehaviour
         }
 
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+
+    public void HandleZoom()
+    {            
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffset.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFollowOffset.y += zoomAmount;
+        }
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
     }
 }
